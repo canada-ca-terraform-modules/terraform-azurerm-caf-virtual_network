25ab718014e8f67cb9f05a2f2071a7fb8a1e1821
bernardmaltais <bernard.maltais@ssc-spc.gc.ca>
2022-07-28 14:23:46 -0400

    Update module.tf

--- module.tf
+++ module.tf
@@ -4,6 +4,5 @@ resource azurerm_virtual_network vnet {
   resource_group_name   = var.resource_group.name
   address_space         = var.address_space
   dns_servers           = var.dns_servers
-  vm_protection_enabled = var.vm_protection_enabled
   tags                  = local.tags
 }
